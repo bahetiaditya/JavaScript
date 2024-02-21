c24c756c7184f9fdf73a839a07ac5859b44bfb55
Aditya Baheti <112819226+bahetiaditya@users.noreply.github.com>
2024-02-21 11:29:37 +0000

    commit in basics

--- 01_BasicsJS/datatypes.js
+++ 01_BasicsJS/datatypes.js
@@ -6,11 +6,11 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Aditya")
 
 
-let name = "hitesh"
-let age = 18
+let name = "Aditya"
+let age = 21
 let isLoggedIn = false
 let state;
 
--- 01_BasicsJS/variables.js
+++ 01_BasicsJS/variables.js
@@ -1,15 +1,15 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "aditya@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Nanded"
 let accountState;
 
 // accountId = 2 // not allowed
 
 
-accountEmail = "hc@hc.com"
+accountEmail = "ab@hc.com"
 accountPassword = "21212121"
-accountCity = "Bengaluru"
+accountCity = "manwath"
 
 console.log(accountId);
 
