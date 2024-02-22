a1932868796327e01c41efe25c3e05feddad723a
Aditya Baheti <112819226+bahetiaditya@users.noreply.github.com>
2024-02-22 04:31:29 +0000

    commit on main

---
+++ 01_BasicsJS/strings.js
@@ -0,0 +1,25 @@
+// const name = "Aditya"
+// const repoCount = 50
+
+// console.log(name + " "+ repoCount + "VALUE")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Adityasb')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+const newString = gameName.substring(0,4);
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 2);
+
+// console.log(anotherString);
+
+const newStringOne = "   Aditya   "
+
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+ 
